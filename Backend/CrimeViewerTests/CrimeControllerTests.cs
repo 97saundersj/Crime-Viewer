a0fc001fb71cdd56c584070cbaf95b03ef2bc7dd
97SaundersJ <jacques_s@hotmail.co.uk>
2024-04-06 17:40:52 +0100

    Added controller & Tests for Crime Viewer

---
+++ Backend/CrimeViewerTests/CrimeControllerTests.cs
@@ -0,0 +1,60 @@
+﻿using CrimeSummaryService.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using Moq.Protected;
+using PoliceCrimeViewer;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace CrimeSummaryService.Tests
+{
+    [TestClass]
+    public class CrimeSummaryControllerTests
+    {
+        [TestMethod]
+        public async Task GetCrimeSummary_Returns_Successful_Response()
+        {
+            // Arrange
+            var mockHttpMessageHandler = new Mock<HttpMessageHandler>();
+            mockHttpMessageHandler
+                .Protected()
+                .Setup<Task<HttpResponseMessage>>("SendAsync", ItExpr.IsAny<HttpRequestMessage>(), ItExpr.IsAny<CancellationToken>())
+                .ReturnsAsync(new HttpResponseMessage(HttpStatusCode.OK)
+                {
+                    Content = new StringContent("[{\"category\":\"Burglary\"},{\"category\":\"Assault\"}]")
+                });
+
+            var httpClient = new HttpClient(mockHttpMessageHandler.Object);
+            var controller = new CrimeSummaryController(httpClient);
+
+            // Act
+            var crimeSummary = await controller.GetCrimeSummary(51.44237, -2.49810, 1);
+
+            // Assert
+            Assert.IsNotNull(crimeSummary);
+            Assert.AreEqual(2, crimeSummary.Count);
+        }
+
+        [TestMethod]
+        public async Task GetCrimeSummary_Returns_Error_When_Api_Fails()
+        {
+            // Arrange
+            var mockHttpMessageHandler = new Mock<HttpMessageHandler>();
+            mockHttpMessageHandler
+                .Protected()
+                .Setup<Task<HttpResponseMessage>>("SendAsync", ItExpr.IsAny<HttpRequestMessage>(), ItExpr.IsAny<CancellationToken>())
+                .ReturnsAsync(new HttpResponseMessage(HttpStatusCode.InternalServerError));
+
+            var httpClient = new HttpClient(mockHttpMessageHandler.Object);
+            var controller = new CrimeSummaryController(httpClient);
+
+            // Act & Assert
+            var exception = await Assert.ThrowsExceptionAsync<Exception>(() => controller.GetCrimeSummary(51.44237, -2.49810, 1));
+            Assert.AreEqual(HttpStatusCode.InternalServerError.ToString(), exception.Message);
+        }
+    }
+}
---
+++ Backend/PoliceCrimeViewer/Controllers/CrimeController.cs
@@ -0,0 +1,62 @@
+﻿using System.Net.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using PoliceCrimeViewer;
+
+namespace CrimeSummaryService.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CrimeSummaryController : ControllerBase
+    {
+        private readonly HttpClient _httpClient;
+        //private readonly HttpMessageHandler _httpMessageHandler;
+
+        public CrimeSummaryController(HttpClient httpClient)
+        {
+            //_httpMessageHandler = httpMessageHandler;
+            _httpClient = httpClient;
+        }
+
+        [HttpGet]
+        public async Task<List<CrimeSummary>> GetCrimeSummary(double lat, double lng, int month)
+        {
+            try
+            {
+                using (_httpClient)//(var httpClient = new HttpClient(_httpMessageHandler))
+                {
+                    DateTime currentDate = DateTime.Now;
+                    DateTime requestedDate = new DateTime(currentDate.Year - (month <= currentDate.Month ? 0 : 1), month, 1);
+
+                    string crimeDataUrl = $"https://data.police.uk/api/crimes-street/all-crime?lat={lat}&lng={lng}&date={requestedDate:yyyy-MM}";
+                    var response = await _httpClient.GetAsync(crimeDataUrl);
+
+                    if (!response.IsSuccessStatusCode)
+                        throw new Exception(response.StatusCode.ToString());
+
+                    var crimeData = JsonConvert.DeserializeObject<List<Crime>>(await response.Content.ReadAsStringAsync());
+
+                    // Group crimes by category
+                    var crimeSummary = crimeData.GroupBy(c => c.Category)
+                                                .Select(group => new CrimeSummary(group.Key, group.Count())).ToList();
+
+                    return crimeSummary;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+
+    public class Crime
+    {
+        public string Category { get; set; }
+        // Add other properties as needed
+    }
+}
