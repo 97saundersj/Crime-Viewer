d3c22b34e29891df2e30d64c715068d21151fd90
97SaundersJ <jacques_s@hotmail.co.uk>
2024-04-07 19:44:16 +0100

    Updated workflow files, added error catching to frontend api calls

--- Backend/Crime-Viewer-FrontEnd/Service/ApiService.cs
+++ Backend/Crime-Viewer-FrontEnd/Service/ApiService.cs
@@ -2,22 +2,31 @@
 
 public class ApiService
 {
-    private readonly IConfiguration _configuration;
-    private readonly HttpClient _httpClient;
+	private readonly IConfiguration _configuration;
+	private readonly HttpClient _httpClient;
 
-    public ApiService(IConfiguration configuration, HttpClient httpClient)
-    {
-        _configuration = configuration;
-        _httpClient = httpClient;
-    }
+	public ApiService(IConfiguration configuration, HttpClient httpClient)
+	{
+		_configuration = configuration;
+		_httpClient = httpClient;
+	}
 
-    public async Task<string> GetTest()
-    {
-        string baseUrl = _configuration["ApiBaseUrl"];
+	public async Task<string> GetTest()
+	{
+		string responseBody = null;
+		try
+		{
+			string baseUrl = _configuration["ApiBaseUrl"];
 
-        var response = await _httpClient.GetAsync($"{baseUrl}crime/test");
-        response.EnsureSuccessStatusCode();
-        var responseBody = await response.Content.ReadAsStringAsync();
-        return responseBody;//JsonSerializer.Deserialize<string>(responseBody);
-    }
+			var response = await _httpClient.GetAsync($"{baseUrl}crime/test");
+			response.EnsureSuccessStatusCode();
+			responseBody = await response.Content.ReadAsStringAsync();
+		}
+		catch (Exception ex)
+		{
+			responseBody = ex.Message;
+		}
+
+		return responseBody;//JsonSerializer.Deserialize<string>(responseBody);
+	}
 }
