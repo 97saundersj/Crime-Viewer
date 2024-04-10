25dadb77bbe98cfd4688dd10ec53d86097dc02cf
Jacques Saunders <jacques_s@hotmail.co.uk>
2024-04-10 22:10:10 +0100

    Development (#4)
    * Edited program.cs to always use swagger
    
    * Added Demo Unit Tests
    
    * Update main_crimeviewer.yml
    
    * Added controller & Tests for Crime Viewer
    
    * Added PoliceUK Package
    
    * Removed Example controller and Tests
    
    * Updated Github Action Workflow name
    
    * Added Basic Blazor Frontend
    
    * Update main_crimeviewer.yml to deploy web app
    
    * Added front end workflow
    
    * set api workflow app-name, provided correct filepaths for frontend
    
    * Provided frontend workflow publish file location
    
    * specifying correct app-name for frontend
    
    * Edited appsettings.development to use deployed api location
    
    * Updated workflow files, added error catching to frontend api calls
    
    * Added more logging info
    
    * Changed frontedn workflow to only build frontend project
    
    * Updated backend workflow to only build, test, and deploy relevant projects, removed error logging from frontend api service
    
    * Specified api tests as a seperate step in the workflow
    
    * Removed multiple projects from Backend build, Updated nav icon
    
    * Made the local crime page the default page
    
    * Update README.md
    
    * Added basic inputs
    
    * Added Search functionality
    
    * Added User Location button
    
    * FIxed Date Search
    
    * Added last updated
    
    * Moved around components
    
    * Reformatted Project structure
    
    * Updated YAML workflows
    
    * Formatted Table
    
    * Removed Test rest endpoint/request
    
    * Commented out failing code

---
+++ Projects/Crime-Viewer-FrontEnd/Service/ApiService.cs
@@ -0,0 +1,61 @@
+﻿using PoliceUk.Entities.StreetLevel;
+using System.Text.Json;
+
+public class ApiService
+{
+	private readonly IConfiguration _configuration;
+	private readonly HttpClient _httpClient;
+	private readonly string? _baseUrl;
+
+	public ApiService(IConfiguration configuration, HttpClient httpClient)
+	{
+		_configuration = configuration;
+		_httpClient = httpClient;
+
+		_baseUrl = _configuration["ApiBaseUrl"];
+	}
+
+	public async Task<DateTime> GetLastUpdated()
+	{
+		try
+		{
+			var response = await _httpClient.GetAsync($"{_baseUrl}crime/lastupdated");
+			response.EnsureSuccessStatusCode();
+			var responseBody = await response.Content.ReadAsStringAsync();
+
+			// Deserialize the response into StreetLevelCrimeResults object
+			// TODO: Move PropertyNameCaseInsensitive into Program?
+			var summary = JsonSerializer.Deserialize<DateTime>(responseBody, new JsonSerializerOptions
+			{
+				PropertyNameCaseInsensitive = true
+			});
+			return summary;
+		}
+		catch (Exception ex)
+		{
+			throw new Exception("Failed to retrieve crime summary data from the API.", ex);
+		}
+	}
+
+	public async Task<StreetLevelCrimeResults> GetCrimeSummary(double latitude, double longitude, int? month = null)
+    {
+        try
+        {
+            var response = await _httpClient.GetAsync($"{_baseUrl}crime/summary?lat={latitude}&lng={longitude}&month={month}");
+            response.EnsureSuccessStatusCode();
+            var responseBody = await response.Content.ReadAsStringAsync();
+
+			// Deserialize the response into StreetLevelCrimeResults object
+			// TODO: Move PropertyNameCaseInsensitive into Program?
+			var summary = JsonSerializer.Deserialize<StreetLevelCrimeResults>(responseBody, new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true
+            });
+            return summary;
+        }
+        catch (Exception ex)
+        {
+            throw new Exception("Failed to retrieve crime summary data from the API.", ex);
+        }
+    }
+}
---
+++ Projects/PoliceCrimeViewer/Controllers/CrimeController.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PoliceUk.Entities.StreetLevel;
+using PoliceUk;
+
+namespace CrimeSummaryService.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CrimeController : ControllerBase
+    {
+        private readonly IPoliceUkClient PoliceClient;
+
+        private static DateTime? CrimesLastUpdated = null;
+
+        public CrimeController(IPoliceUkClient policeClient)
+        {
+            PoliceClient = policeClient;
+        }
+
+        [HttpGet("lastUpdated")]
+        public DateTime GetLastUpdated()
+        {
+            try
+            {
+                // Can't get from Interface so cast to class - Raise as issue on PoliceUKClient Github?
+                CrimesLastUpdated = ((PoliceUkClient)PoliceClient).LastUpdated();
+                return CrimesLastUpdated.Value;
+
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Failed to retrieve crime data from Police UK API.", ex);
+            }
+        }
+
+        [HttpGet("summary")]
+        public StreetLevelCrimeResults GetCrimeSummary(double lat, double lng, int? month = null)
+        {
+            try
+            {
+                var geoposition = new Geoposition(lat, lng);
+                DateTime? requestedDate = month.HasValue ? ConvertMonthToDateTime(month.Value) : null;
+
+                var streetLevelCrimes = PoliceClient.StreetLevelCrimes(geoposition, requestedDate);
+
+                return streetLevelCrimes;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Failed to retrieve crime data from Police UK API.", ex);
+            }
+        }
+
+        private DateTime UpdateLastUpdated()
+        {
+            try
+            {
+                // Can't get from Interface so cast to class - Raise as issue on PoliceUKClient Github?
+                CrimesLastUpdated = ((PoliceUkClient)PoliceClient).LastUpdated();
+                return CrimesLastUpdated.Value;
+
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Failed to retrieve crime data from Police UK API.", ex);
+            }
+        }
+
+        private DateTime ConvertMonthToDateTime(int monthNumber)
+        {
+            int currentYear = DateTime.Now.Year;
+            int currentMonth = DateTime.Now.Month;
+
+            if (!CrimesLastUpdated.HasValue)
+            {
+                UpdateLastUpdated();
+
+            }
+            if (monthNumber > CrimesLastUpdated.Value.Month)
+            {
+                // The requested month is in the past, use the previous year
+                return new DateTime(currentYear - 1, monthNumber, 1);
+            }
+            else
+            {
+                // The requested month is in the current year
+                return new DateTime(currentYear, monthNumber, 1);
+            }
+        }
+    }
+}
