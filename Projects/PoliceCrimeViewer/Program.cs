54b54a4d2cb74afbea3064ecb6a95628f8b6d9aa
Jacques Saunders <jacques_s@hotmail.co.uk>
2024-04-12 08:48:20 +0100

    Created CrimeService (#5)
    * Added Crime Service
    
    * Added Unit tests for Service
    
    * Updated Text to be more Human readable

--- Projects/PoliceCrimeViewer/Program.cs
+++ Projects/PoliceCrimeViewer/Program.cs
@@ -1,10 +1,11 @@
+using CrimeViewerBackend.Services;
 using PoliceUk;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddSingleton<IPoliceUkClient, PoliceUkClient>();
-builder.Services.AddControllers();
+builder.Services.AddSingleton<ICrimeService, CrimeService>();
 builder.Services.AddControllers();
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
---
+++ Projects/PoliceCrimeViewer/Services/ICrimeService.cs
@@ -0,0 +1,23 @@
+﻿using PoliceUk.Entities.StreetLevel;
+
+namespace CrimeViewerBackend.Services
+{
+    public interface ICrimeService
+{
+        /// <summary>
+        /// Get the Date that the crime data was last updated at.
+        /// </summary>
+        /// <returns><see cref="DateTime"/> representing the last updated date.</returns>
+        public DateTime GetLastUpdated();
+
+        /// <summary>
+        /// Gets a list of crimes in a 1 mile radius of the location specified
+        /// </summary>
+        /// <param name="lat">The latitude of the location.</param>
+        /// <param name="lng">The longitude of the location.</param>
+        /// <param name="month">Optional. The month for which to retrieve crimes. If not specified, retrieves crimes for all months.</param>
+        /// <returns><see cref="StreetLevelCrimeResults"/> containing a list of crimes.</returns>
+        public StreetLevelCrimeResults GetCrimes(double lat, double lng, int? month = null);
+
+    }
+}
