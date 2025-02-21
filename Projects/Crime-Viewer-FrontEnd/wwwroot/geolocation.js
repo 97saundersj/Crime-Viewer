86201c63f5b692e11006f601720f548d797b6ba7
97saundersj <jacques_s@hotmail.co.uk>
2025-02-21 12:33:39 +0000

    Attempting getLocation fix

--- Projects/Crime-Viewer-FrontEnd/wwwroot/geolocation.js
+++ Projects/Crime-Viewer-FrontEnd/wwwroot/geolocation.js
@@ -1,9 +1,17 @@
 window.getLocation = function (dotnetHelper) {
+    console.log("Attempting to get geolocation...");
     if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(function (position) {
-            dotnetHelper.invokeMethodAsync('LocationReceived', position.coords.latitude, position.coords.longitude);
-        });
+        navigator.geolocation.getCurrentPosition(
+            function (position) {
+                console.log("Geolocation obtained: ", position.coords.latitude, position.coords.longitude);
+                dotnetHelper.invokeMethodAsync('LocationReceived', position.coords.latitude, position.coords.longitude);
+            },
+            function (error) {
+                console.error("Error obtaining geolocation: ", error.message);
+            },
+            {maximumAge:10000, timeout:5000, enableHighAccuracy:false}
+        );
     } else {
-        dotnetHelper.invokeMethodAsync('LocationError', "Geolocation is not supported by this browser.");
+        console.log("Geolocation is not supported by this browser.");
     }
 };
